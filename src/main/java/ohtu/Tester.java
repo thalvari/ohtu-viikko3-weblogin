3064dd2f6d2a76a576a3ea1e94699220a45ce44d
thalvari <tuomas.halvari@helsinki.fi>
2018-11-18 19:06:47 +0200

    -

--- src/main/java/ohtu/Tester.java
+++ src/main/java/ohtu/Tester.java
@@ -1,35 +1,88 @@
 package ohtu;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.Random;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriver driver = new HtmlUnitDriver();
+//        WebDriver driver = new HtmlUnitDriver();
+        ChromeDriverManager.getInstance().setup();
+        WebDriver driver = new ChromeDriver();
+        WebElement element;
 
         driver.get("http://localhost:4567");
-
         sleep(2);
-
-        WebElement element = driver.findElement(By.linkText("login"));
+        element = driver.findElement(By.linkText("login"));
         element.click();
-
         sleep(2);
-
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
-
         sleep(2);
         element.submit();
 
-        sleep(3);
+        sleep(2);
+        driver.get("http://localhost:4567");
+        sleep(2);
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+        sleep(2);
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("test");
+        element = driver.findElement(By.name("login"));
+        sleep(2);
+        element.submit();
 
+        sleep(2);
+        driver.get("http://localhost:4567");
+        sleep(2);
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+        sleep(2);
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka1");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("test");
+        element = driver.findElement(By.name("login"));
+        sleep(2);
+        element.submit();
+
+        sleep(2);
+        driver.get("http://localhost:4567");
+        sleep(2);
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        sleep(2);
+        Random r = new Random();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("test" + r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("test");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("test");
+        element = driver.findElement(By.name("signup"));
+        sleep(2);
+        element.submit();
+
+        sleep(2);
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        sleep(2);
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        sleep(3);
         driver.quit();
     }
 
